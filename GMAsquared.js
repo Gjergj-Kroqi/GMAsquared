6ddab2327ccf0741e700453224f26c17ca388a61
Gjergj Kroqi <114444907+Gjergj-Kroqi@users.noreply.github.com>
2022-12-01 23:11:23 -0500

    Add files via upload

--- GMAsquared.js
+++ GMAsquared.js
@@ -1,4 +1,8 @@
+let n;
+
 function game(diff) {
+
+    n = diff;
 
     document.getElementById("header1").innerHTML = "GAME ON";
 
@@ -17,17 +21,55 @@ function game(diff) {
 
     document.getElementById("inputArea").hidden = false;
 
-    x = checkGuess();
-
 
 
 }
 
 function checkGuess() {
     let x = document.getElementById("num").value;
-    alert(x);
+    let correctNum;
+    let condition;
 
-    return x;
+    if (n == 0) {
+        if (x >= 1 && x <= 10) {
+            condition = true;
+        } else {
+            condition = false
+        }
+    } else if (n == 1) {
+        if (x >= 1 && x <= 30) {
+            condition = true;
+        } else {
+            condition = false
+        }
+    } else {
+        if (x >= 1 && x <= 50) {
+            condition = true;
+        } else {
+            condition = false
+        }
+    }
+
+    if (condition == true) {
+        if (n == 0) {
+            correctNum = Math.floor(Math.random() * 10) + 1;
+        } else if (n == 1) {
+            correctNum = Math.floor(Math.random() * 20) + 1;
+        } else {
+            correctNum = Math.floor(Math.random() * 30) + 1;
+        }
+
+        alert("The Correct Number Was "+ correctNum);
+
+        if (x == correctNum) {
+            alert("You guessed correctly");
+        } else {
+            alert("You guessed Incorrectly");
+        }
+    } else {
+        alert("not in the range");
+    }
+
 
 }
 
